84f29b60aa0cd87e2baf248ac91d8ffb5c69ec7c
Jeroen Maes <joenmaes@gmail.com>
2010-12-22 21:46:21 +0100

    Post drink to Twitter/FB

--- public_html/m/drinkDetail.php
+++ public_html/m/drinkDetail.php
@@ -26,6 +26,55 @@ $drink = new Drink(SpoonFilter::getGetValue('id', null, ''));
 if (SpoonFilter::getPostValue('btnAddDrink', null, '') !== "") {
     if($timeAgo < -6)SpoonHTTP::redirect('index.php');
     $latestCheckIn->AddTab(SpoonFilter::getPostValue('drink_id', null, ''));
+
+    /*post to facebook*/
+    $db = new SpoonDatabase('mysql', 'localhost', 'xqdchsmn_public', 'pRAcHU8Ajath7qa3', 'xqdchsmn_public');
+    $user = $db->getRecord('SELECT * FROM users WHERE user_id = ?', SpoonSession::get('id'));
+    $drink_id = SpoonFilter::getPostValue('drink_id', null, '');
+    $drink = $db->getRecord('SELECT * FROM drinks WHERE drink_id = ?', $drink_id);
+
+        if($user['fb_publish_stream']){
+            require_once 'facebook/facebook.php';
+
+            // Create our Application instance (replace this with your appId and secret).
+            $facebook = new Facebook(array(
+                  'appId'  => '118234134911012',
+                  'secret' => 'a83b1fbf766dcf41a8238a13f53690bd',
+                  'cookie' => true,
+                ));
+
+            $uid = SpoonSession::get('id');
+            $db = new SpoonDatabase('mysql', 'localhost', 'xqdchsmn_public', 'pRAcHU8Ajath7qa3', 'xqdchsmn_public');
+            $user = $db->getRecord('SELECT * FROM users WHERE user_id = ?', $uid);
+
+            $fb_uid = $user['fb_uid'];
+            $messageContent = 'I just drank a '.$drink['name'].' - http://publicapp.tk/drinks/'.$drink['drink_id'].'';
+            $facebook->api($fb_uid.'/feed', 'post', array('message'=> $messageContent, 'cb' => ''));
+        }
+
+    /*end*/
+
+    /*post to twitter*/
+        if($user['twitter_uid']){
+            require_once('twitteroauth/twitteroauth.php');
+
+            define('CONSUMER_KEY', '4K5I4iPpEGc4KgTN1VnKDA');
+            define('CONSUMER_SECRET', 'cRWey0CbUXuD0qIrA89s9tKQjHtxQXRn8leR7AiI');
+            define('OAUTH_CALLBACK', 'http://www.publicapp.tk/twittercallback.php');
+
+            $uid = SpoonSession::get('id');
+            $db = new SpoonDatabase('mysql', 'localhost', 'xqdchsmn_public', 'pRAcHU8Ajath7qa3', 'xqdchsmn_public');
+            $user = $db->getRecord('SELECT * FROM users WHERE user_id = ?', $uid);
+            $twitter_token = $user['twitter_token'];
+            $twitter_secret = $user['twitter_secret'];
+            $messageContent = 'I just drank a '.$drink['name'].' - http://publicapp.tk/drinks/'.$drink['drink_id'].'';
+
+
+            $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $twitter_token, $twitter_secret);
+            $response = $connection->post('statuses/update', array('status' => $messageContent));
+        }
+    /*end*/
+
     SpoonHTTP::redirect('checkin.php');
 }
 
