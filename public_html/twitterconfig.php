2318afc98256fa9a86a95f53b1ab70b1fb741367
Jeroen Maes <joenmaes@gmail.com>
2010-12-22 16:31:11 +0100

    Dashboard settings

---
+++ public_html/twitterconfig.php
@@ -0,0 +1,7 @@
+<?php
+
+define('CONSUMER_KEY', '4K5I4iPpEGc4KgTN1VnKDA');
+define('CONSUMER_SECRET', 'cRWey0CbUXuD0qIrA89s9tKQjHtxQXRn8leR7AiI');
+define('OAUTH_CALLBACK', 'http://www.publicapp.tk/twittercallback.php');
+
+?>
