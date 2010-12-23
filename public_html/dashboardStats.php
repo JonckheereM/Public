4cc60611f6d2b2e51ae3b35bc38ae2cb83ce7c45
Jeroen Maes <joenmaes@gmail.com>
2010-12-23 15:54:38 +0100

    Dashboard Stats

--- public_html/dashboardStats.php
+++ public_html/dashboardStats.php
@@ -18,18 +18,26 @@
         $tpl->assign('oNavMe', true);
 
         $uid = SpoonSession::get('public_uid');
+
         $db = new SpoonDatabase('mysql', 'localhost', 'xqdchsmn_public', 'pRAcHU8Ajath7qa3', 'xqdchsmn_public');
-        $user = $db->getRecord('SELECT * FROM users WHERE user_id = ?', $uid);
+        $checkins = $db->getRecords('SELECT * FROM checkins WHERE user_id = ?', $uid);
 
-         $tpl->assign('uname', $user['username']);
-         $tpl->assign('fbu', $user['fb_uid']);
+        $user = new User($uid);
 
-         $lastChecking = $db->getRecord('SELECT * FROM checkins WHERE user_id = ? ORDER BY timestamp DESC', $uid);
-         $lastPub = $db->getRecord('SELECT * FROM pubs WHERE pub_id = ?', $lastChecking['pub_id']);
+        $tpl->assign('daysActive', 'NOT ENOUGH DATA');
 
-         $tpl->assign('lastPub', $lastPub['name']);
-         $tpl->assign('lastPubId', $lastPub['pub_id']);
-         $tpl->assign('lastDate', SpoonDate::getTimeAgo(strtotime($lastChecking['timestamp'])));
+        $tpl->assign('checkins', count($checkins));
+
+        $tpl->assign('avgDrinks', 'NOT ENOUGH DATA');
+        
+        $tpl->assign('avgDay', 'NOT ENOUGH DATA');
+
+        $tpl->assign('topFriends', 'NOT ENOUGH DATA');
+
+        if($user->GetTopPubs(5) !== null){
+        $tpl->assign('oTopPubs', true);
+            $tpl->assign('iTopPubs', $user->GetTopPubs(5));
+        }else $tpl->assign('oNoTopPubs', true);
 
     }else{ //GTFO!!!
         SpoonHTTP::redirect('index.php');
--- public_html/templates/dashboardStats.tpl
+++ public_html/templates/dashboardStats.tpl
@@ -18,8 +18,37 @@
 
 <div id="content" class="fluid">
     <div class="container">
-
+        <div id="stats">
         <h2>Stats</h2>
+        
+        <a href="#" id="tweet">+ Tweet these stats!</a>
 
+        <h3>DAYS OUT DRINKING</h3>
+        {$daysActive}
+
+        <h3>NUMBER OF CHECKINS</h3>
+        {$checkins}
+
+        <h3>AVG DRINKS PER PUB</h3>
+        {$avgDrinks}
+
+        <h3>AVG PUBS WHEN OUT DRINKING</h3>
+        {$avgDay}
+
+        <h3>TOP 5 FRIENDS</h3>
+        {$topFriends}
+
+        <h3>TOP 5 PUBS</h3>
+        {option:oTopPubs}
+            <ul>
+                {iteration:iTopPubs}
+                <li>+ <a href="/pubs/{$iTopPubs.pub_id}">{$iTopPubs.name}</a> <span>{$iTopPubs.count} checkins</span></li>
+                {/iteration:iTopPubs}
+            </ul>
+        {/option:oTopPubs}
+        {option:oNoTopPubs}
+        <p>No top pubs.</p>
+        {/option:oNoTopPubs}
+        </div>
     </div>
 </div>
